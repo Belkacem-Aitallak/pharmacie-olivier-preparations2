c7d103bd815beecd28c65ee7f81e3753050dc684
Belkacem <belkacemaitallak@gmail.com>
2026-03-18 10:26:04 +0100

    add medicaments routes

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
 
+// données exemple
+let medicaments = [
+  { id: 1, nom: "Paracetamol", stock: 120 },
+  { id: 2, nom: "Ibuprofene", stock: 80 }
+]
+
+// route principale
 app.get("/", (req, res) => {
   res.send("Pharmacie Olivier API running 🚀")
+})
+
+// liste medicaments
+app.get("/medicaments", (req, res) => {
+  res.json(medicaments)
+})
+
+// ajouter medicament
+app.post("/medicaments", (req, res) => {
+  const medicament = req.body
+  medicaments.push(medicament)
+  res.json({ message: "Medicament ajouté" })
 })
 
 const PORT = process.env.PORT || 3000
